d718fcce8049e4135bc350fd0980718a7cab439b
ingJuanArenas <juanarenasing@gmail.com>
2025-12-31 10:20:27 -0500

    using eager and lazy fetch

--- src/main/java/com/pizza/persistence/model/Order.java
+++ src/main/java/com/pizza/persistence/model/Order.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,10 +37,11 @@ public class Order {
     @Column(name="order_date", nullable = false, columnDefinition = "TIMESTAMP")
     private LocalDate orderDate;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "client_id", insertable = false, updatable = false)
+    @JsonIgnore
     private Client client;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", fetch = FetchType.EAGER)
     private List<DetailsOrder> detailsOrders;
 }
