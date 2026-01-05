2df5d473e5f130649a5b7068ac037574d1b59ad5
ingJuanArenas <juanarenasing@gmail.com>
2026-01-05 09:42:31 -0500

    cleaning unused imports

--- src/main/java/com/pizza/persistence/model/Pizza.java
+++ src/main/java/com/pizza/persistence/model/Pizza.java
@@ -1,16 +1,12 @@
 package com.pizza.persistence.model;
 
 
-import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
